3af3b8b4610a98c87af5e8a6d72cf3fda8ca4151
instr3 <instr3@163.com>
2016-08-13 13:10:17 +0800

    First commit

---
+++ Assets/Main.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour
+{
+
+    public SensorShower[] ShowList;
+    public Transform FollowerTransform;
+    private Vector3 PreviousAcc = new Vector3();
+    private Vector3 Speed = new Vector3();
+    void Start()
+    {
+        Input.gyro.enabled = true;
+        Input.compass.enabled = true;
+    }
+    void Update()
+    {
+        ShowList[0].Value = Input.gyro.userAcceleration * 10;
+        ShowList[0].SetTitle("Gyro.UserAcceleration");
+        ShowList[1].Value = Input.acceleration;
+        ShowList[1].SetTitle("Acceleration");
+        ShowList[2].Value = Input.gyro.gravity;
+        ShowList[2].SetTitle("Gyro.Gravity");
+        ShowList[3].Value = Input.gyro.rotationRate;
+        ShowList[3].SetTitle("Gyro.RotationRate");
+        //ShowList[4].Value = new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Jump"), Input.GetAxis("Horizontal"));
+        //ShowList[4].SetTitle("Key");
+        ShowList[5].Value = new Vector3(Input.compass.trueHeading / 180f, 0, 0);
+        ShowList[5].SetTitle("Compass.trueHeading");
+        ShowList[4].Value = Input.gyro.attitude.eulerAngles/180f;
+        ShowList[4].SetTitle("gyro.attitude");
+        //Camera.main.transform.rotation = Input.gyro.attitude;
+        //PreviousAcc = Vector3.Lerp(PreviousAcc, Input.gyro.userAcceleration, 0.02f);
+        //FollowerTransform.position =
+        //Camera.main.transform.TransformVector(-PreviousAcc * 2);
+        //Speed += Camera.main.transform.TransformVector(Input.gyro.userAcceleration);
+        //Speed += Vector3.ClampMagnitude(-FollowerTransform.position, 5f) * 0.1f;
+        //Speed *= 0.9f;
+        //FollowerTransform.position += Speed * Time.deltaTime;
+    }
+    void FixedUpdate()
+    {
+        PreviousAcc = Vector3.Lerp(PreviousAcc, Input.gyro.userAcceleration, 0.02f);
+        FollowerTransform.position = Camera.main.transform.TransformVector(-PreviousAcc*10);
+
+    }
+}
---
+++ Assets/SensorShower.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SensorShower : MonoBehaviour
+{
+    private RectTransform XButton, YButton, ZButton;
+    private Text XText, YText, ZText;
+    private float height = 18;
+    [HideInInspector]
+    public Vector3 Value = new Vector3(-0.5f, 0.333333f, 1f);
+    // Use this for initialization
+    void Start()
+    {
+        Text XLabel, YLabel, ZLabel;
+        XLabel = transform.FindChild("XLabel").GetComponent<Text>();
+        YLabel = transform.FindChild("YLabel").GetComponent<Text>();
+        ZLabel = transform.FindChild("ZLabel").GetComponent<Text>();
+        XButton = XLabel.GetComponentInChildren<Button>().GetComponent<RectTransform>();
+        YButton = YLabel.GetComponentInChildren<Button>().GetComponent<RectTransform>();
+        ZButton = ZLabel.GetComponentInChildren<Button>().GetComponent<RectTransform>();
+        XText = XButton.GetComponentInChildren<Text>();
+        YText = YButton.GetComponentInChildren<Text>();
+        ZText = ZButton.GetComponentInChildren<Text>();
+    }
+    void SetSize(RectTransform rect,float x)
+    {
+        float length = 40f * x + 60f;
+        rect.sizeDelta = new Vector2(length, height);
+        rect.localPosition = new Vector3(length / 2 + 24, 0, 0);
+    }
+    public void SetTitle(string text)
+    {
+        GetComponent<Text>().text = text;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        XText.text = Value.x.ToString("F2");
+        YText.text = Value.y.ToString("F2");
+        ZText.text = Value.z.ToString("F2");
+        SetSize(XButton, Value.x);
+        SetSize(YButton, Value.y);
+        SetSize(ZButton, Value.z);
+        Vector3 test = 40f * Value + new Vector3(60f, 60f, 60f);
+        XButton.sizeDelta = new Vector2(Mathf.Clamp(test.x, 20f, 100f), height);
+        YButton.sizeDelta = new Vector2(Mathf.Clamp(test.y, 20f, 100f), height);
+        ZButton.sizeDelta = new Vector2(Mathf.Clamp(test.z, 20f, 100f), height);
+    }
+}
